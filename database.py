677c7c64a7a2ec920873c566f4bed430bb7ee6b2
grpxstm <115970058+grpxstm@users.noreply.github.com>
2023-02-27 11:36:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 DETA_KEY = "d0uykfhmbwh_bA8MJRAaQNc94fALykvCe7Mqm57ZTHD2" 
 deta = Deta(DETA_KEY)
-db = deta.Base("users_db")
+dbs = deta.Base("users_db")
 def insert_user(username, name, password):
 	return db.put({"key": username, "name":name, "password": password})
 
 insert_user("Rishwin", "rishwin r kanth", "rishwin123")
 
 def fetch_all_users():
-	res = db.fetch()
+	res = dbs.fetch()
 	return res.items
 
 print(fetch_all_users())
